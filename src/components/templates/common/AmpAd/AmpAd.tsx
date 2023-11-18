42c6687e2dd34ffea0392c400d75a6d8063574dc
hbnationit <hbnation.it@gmail.com>
2023-11-18 22:23:07 +0900

    ENH: amp-ad to ins tag

--- src/components/templates/common/AmpAd/AmpAd.tsx
+++ src/components/templates/common/AmpAd/AmpAd.tsx
@@ -1,19 +1,40 @@
-import React from 'react'
+'use client'
+
+import React, { useEffect } from 'react'
 
 export default function AmpAd() {
+  useEffect(() => {
+    const pushAd = () => {
+      try {
+        // @ts-ignore
+        const { adsbygoogle } = window
+        adsbygoogle.push({})
+      } catch (e) {
+        console.error(e)
+      }
+    }
+
+    const interval = setInterval(() => {
+      // @ts-ignore
+      if (window.adsbygoogle) {
+        pushAd()
+        clearInterval(interval)
+      }
+    }, 300)
+
+    return () => {
+      clearInterval(interval)
+    }
+  }, [])
+
   return (
-    <amp-ad
-      width="100vw"
-      height="320"
-      type="adsense"
+    <ins
+      className="adsbygoogle"
+      style={{ display: 'block' }}
       data-ad-client="ca-pub-4998422659731294"
       data-ad-slot="6878857551"
-      data-auto-format="rspv"
-      data-full-width=""
-    >
-      {/*
-      // @ts-ignore */}
-      <div overflow="" />
-    </amp-ad>
+      data-ad-format="auto"
+      data-full-width-responsive="true"
+    />
   )
 }
